e5f48f61f3719e891cd268305c8ecb4415fc77c7
bsh00699 <525534397@qq.com>
2022-08-12 20:38:36 +0800

    feat: modify assert test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,11 +5,13 @@ const expect = chai.expect;
 
 describe('Test webpack.base.js', () => {
   const baseCfg = require('../../lib/webpack.base.js')
-  // console.log('baseCfg', baseCfg);
+  console.log('baseCfg', baseCfg);
   it('entry', () => {
     // assert.equal(baseCfg.entry.index, '/Users/xingchao/Documents/work/builder-webpack-tr/test/smoke/template/src/index/index.js')
-    expect(baseCfg.entry.index).to.equal('/Users/xingchao/Documents/work/builder-webpack-tr/test/smoke/template/src/index/index.js')
     expect(baseCfg).to.have.property('entry')
-    expect(baseCfg).to.have.property('entry')
+    expect(baseCfg).to.have.property('output')
+    expect(baseCfg).to.have.property('module')
+    expect(baseCfg).to.have.property('plugins')
+    expect(baseCfg).to.have.property('stats')
   })
 })
